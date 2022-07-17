f06a1c50283b7006b77cdc0f6ff7e485b77e3c33
iballi2017 <iballi201@gmail.com>
2022-07-18 00:01:07 +0100

    implements 'question-type-options' page

---
+++ pages/question-type-options.php
@@ -0,0 +1,74 @@
+<!-- Header -->
+<?php include "../components/header.php"; ?>
+
+
+<div id="question-type-options-page">
+    <div class="page-wrapper school-bg py-5 pb-3">
+        <div class="inner container-fluid">
+            <!--  -->
+            <div id="toolbar">
+                <ul class="inner-wrapper list-style-none pl-0 mb-0">
+                    <li>
+                        <ul id="social-handles" class="list-style-none pl-0 mb-0">
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/images/foundation_social-facebook.png" alt="" class="img-fluid">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/images/foundation_social-twitter.png" alt="" class="img-fluid">
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#">
+                                    <img src="../assets/images/foundation_social-instagram.png" alt="" class="img-fluid">
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <h1 class="text-uppercase">Tep Digital LRP</h1>
+                    </li>
+                    <li class="account-link">
+                        <div id="user-account-avatar">
+                            <div class="image-wrapper mx-auto">
+                                <img src="../assets/images/material-symbols_account-box.png" alt="" class="img-fluid">
+                            </div>
+                            <p class=" mx-auto">ACCOUNT</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div id="content">
+                <div class="content-wrapper">
+                    <div class="content-label">
+                        <h2>CHOOSE THE TYPE OF QUESTION</h2>
+                    </div>
+                    <ul class="type-option list-style-none pl-0 mb-0">
+                        <li>
+                            <div class="card">
+                                <img src="../assets/images/literacy-featured-image.png" class="card-img-top" alt="literacy featured image">
+                                <div class="card-body text-center">
+                                    <p class="card-text">Literacy</p>
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <div class="card">
+                                <img src="../assets/images/numeracy-featured-image.png" class="card-img-top" alt="numeracy featured image">
+                                <div class="card-body text-center">
+                                    <p class="card-text">Numeracy</p>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Footer -->
+<?php include "../components/footer.php"; ?>
