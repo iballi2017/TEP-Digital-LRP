f8ce92ada78fb4f226439001e3ee80297cbfe0cc
allizwarttech <91635445+allizwarttech@users.noreply.github.com>
2022-07-19 20:48:38 +0100

    new commit

---
+++ components/levels-navigation.php
@@ -0,0 +1,32 @@
+<div id="levels-navigation">
+    <div class="main-wrapper">
+        <div class="title text-center mb-4">Level</div>
+        <ul id="navigation-items">
+            <li class="active">
+                <a href="#">
+                    LETTER 1
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    LETTER 2
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    WORD
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    PARAGRAPH
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    STORY
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
